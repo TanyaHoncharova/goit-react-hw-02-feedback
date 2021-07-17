4e3acb89ae76c71a8accc60bc7d3c3d4560fabd8
TanyaHoncharova <tanushaGK@gmail.com>
2021-07-17 12:46:17 +0300

    step 4

--- src/components/Counter/FeedbackOptions.jsx
+++ src/components/Counter/FeedbackOptions.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 
 const FeedbackOptions = ({onHandleGoodBtn, onHandleNeutralBtn, onHandleBadBtn}) => {
     return (
-                    <div className="StatCounter">
-                <h2 className="title">Please leave feedback</h2>
-                <div className="BtnContainer">
+            <div className="BtnContainer">
                     <button type="button" onClick={onHandleGoodBtn} className="statBtn"> Good</button>
                 <button type="button" onClick={onHandleNeutralBtn} className="statBtn"> Neutral</button>
                 <button type="button" onClick={onHandleBadBtn} className="statBtn"> Bad </button>
-            </div>
             </div>
     )
 };
--- src/components/Counter/Statistics.jsx
+++ src/components/Counter/Statistics.jsx
@@ -2,19 +2,20 @@ import React  from 'react';
 
 const Statistics = ({Good, Neutral, Bad, Total, Positive})=>{
     return(
-        <div>
-        <h2 className="title" >Statistics</h2>
+        <>
     <ul className="statistic">
         <li>Good: {Good} </li>
         <li> Neutral: {Neutral} </li>
-        <li>Bad: {Bad} </li>
-        <li>Total: {Total()} </li>
-        <li>Positive feedback: {Positive()} % </li>
-    </ul>
-    </div>
-
-    )
-}
+                <li>Bad: {Bad} </li>
+            </ul>
+        {(Total !== 0) ? 
+        <>
+            <p><b>Total feedbacks: {Total}</b></p>
+                    <p>Positive feedbacks: <b>{Positive()}%</b></p>
+                    <h3>Thank you for yours feedback! </h3>
+        </> : "No feedback given"}
+    </>
+)};
 
 
 export default Statistics;
